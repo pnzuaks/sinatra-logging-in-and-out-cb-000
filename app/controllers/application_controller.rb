495d7952d30e9b8401f5a5ade99e37b1192d78d0
Patricia Nykaza <pnykaza@calstatela.edu>
2018-10-05 18:25:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    # @user = User.find(session[:user_id])
+    @user = User.find_by(id: session[:user_id])
     erb :account
   end
 
