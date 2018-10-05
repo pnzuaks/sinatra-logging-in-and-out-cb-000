96375b90337eb739b40c618aae18ab29d675c1e0
Patricia Nykaza <pnykaza@calstatela.edu>
2018-10-05 17:54:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    
     if @user = User.find_by(username: params["username"])
       session[:user_id] = @user.id
       redirect '/account'
