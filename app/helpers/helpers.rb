bedaec09c866558f08fdf466b575424442ee8c0b
Patricia Nykaza <pnykaza@calstatela.edu>
2018-10-05 17:26:59 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,11 @@
 class Helpers
   def current_user(session)
-    @user = session[:user_id]
+    @user = User.find(session[:user_id])
+    @user
   end
 
   def is_logged_in?(session)
-
+    if User.find(session[:user_id])
+    end
   end
 end
